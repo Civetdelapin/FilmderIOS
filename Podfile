884dc9982d2a563ed335a718b1ac53eb0bcf65ff
Nicolas Guevara <nicolas.guevara@utt.fr>
2018-12-18 22:54:59 +0000

    ajout CocoaPods (NON OPERATIONNEL)

---
+++ Podfile
@@ -0,0 +1,8 @@
+#commentaire
+platform :ios, '9.0'
+
+use_frameworks!
+
+target 'Filmder' do
+	pod 'KYDrawerController'
+end
---
+++ Pods/Target Support Files/Pods-Filmder/Pods-Filmder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Filmder : NSObject
+@end
+@implementation PodsDummy_Pods_Filmder
+@end
